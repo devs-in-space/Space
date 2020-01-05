c35bcec81f54239f8854cea543343e7f031c343a
KyleBCox <kylecoxforweb@gmail.com>
2020-01-05 23:24:52 +0000

    adding vga screen writing

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,22 @@
 use core::panic::PanicInfo;
 mod vga_buffer;
 
-static HELLO : &[u8] = b"Hello World!";
-
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-	let vga_buffer =  0xb8000 as *mut u8;
-
-	for (i, &byte) in HELLO.iter().enumerate() {
-		unsafe {
-			*vga_buffer.offset(i as isize * 2) = byte;
-			*vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-
-		}
-	}
+	println!("Hola Bish");
+	println!("Hola Bish");
+	println!("Hola Bish");
+	println!("Hola Bish");
+	println!("Hola Bish");
+	println!("Hola Bish");
+	println!("Hola Bish");
+	println!("Hola Bish");
+	println!("Hola Bish");
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+	println!("{}", _info);
     loop {}
 }
